4ae03a1584ae4a5fc256b7ab69ad9da1a3025dcf
hirono <hotarak@gmail.com>
2017-03-04 11:30:10 +0900

    add setting to application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module Prepare
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+
+    config.generators.template_engine = :slim
+
     config.generators do |g|
       g.test_framework  nil
       g.assets  false
