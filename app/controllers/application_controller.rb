b0777c6b778339f245e58fff6cb47c2c45a24db9
hirono <hotarak@gmail.com>
2017-03-04 15:43:49 +0900

    user iroiro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   add_flash_types :success, :info, :warning, :danger
+end
+
+def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:uid, :provider, :first_name, :last_name, :gender, :birthday])
+  devise_parameter_sanitizer.permit(:account_update, keys: [:uid, :provider, :first_name, :last_name, :gender, :birthday])
 end
