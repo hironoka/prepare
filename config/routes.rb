2a75744d6d2adfe8b257df035d3f4899fff1a02a
hirono <hotarak@gmail.com>
2017-03-04 16:47:20 +0900

    edit routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root "users#index"
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :comments, only: [:create]
+  end
 end
